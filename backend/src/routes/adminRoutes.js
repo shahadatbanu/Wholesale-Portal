b1395f96e69bad96b587ef53345eb449140fa90b
shahadatbanu <sherurangrej783@gmail.com>
2025-04-16 14:59:54 +0530

    added more api

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getAllWholesalers ,deleteUser , getAllProducts } = require("../controllers/adminController");
+const {getAllWholesalers ,deleteUser , getAllProducts , createuser , updateUser , getAllOrders , getOrdersByWholesaler , deleteOrder} = require("../controllers/adminController");
 const { protect, adminOnly } = require("../middlewares/authMiddleware"); // Middleware for auth & role-based access
 
 const router = express.Router();
@@ -8,6 +8,11 @@ router.use(protect, adminOnly);
 router.get("/wholesaler", getAllWholesalers); // Only admins can access this route
 router.delete("/users/:id", deleteUser);
 router.get("/products", getAllProducts);
+router.post("/users", createuser); // Add wholesaler
+router.patch("/users/:id",  updateUser); // Update wholesaler
+router.get("/orders", getAllOrders);
+router.get("/orders/wholesaler/:id", getOrdersByWholesaler);
+router.delete("/orders/:id", deleteOrder);
 
 
 module.exports = router;
